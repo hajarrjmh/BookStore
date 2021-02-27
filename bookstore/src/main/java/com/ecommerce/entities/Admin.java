4dd37fedaeab3e6faff32c1826ce297643bc63a1
latifamkd <latifaelmekdmi@gmail.com>
2021-02-27 04:31:13 +0100

    teeeest

--- bookstore/src/main/java/com/ecommerce/entities/Admin.java
+++ bookstore/src/main/java/com/ecommerce/entities/Admin.java
@@ -21,8 +21,7 @@ public class Admin implements Serializable {
 	@Column(name="admin_id")
   private Long idUser;
 	@Column(name="admin_name")
-  private String userName;
-  private String password;
+  
   private String test;
   
   
@@ -38,28 +37,14 @@ public Long getIdUser() {
 	public void setIdUser(Long idUser) {
 		this.idUser = idUser;
 	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
+	
+	
 	public Admin() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Admin(String userName, String password) {
-		super();
-		this.userName = userName;
-		this.password = password;
-		
-	}
+	
 	
 }
 
